117162e825838fe75deaa2e9a2183973ce73d826
Kaleeswaran V <vkalees64@gmail.com>
2024-11-17 11:26:03 +0530

    fix: schematics fix

--- projects/theme/schematics/add-cdn/index.ts
+++ projects/theme/schematics/add-cdn/index.ts
@@ -1,12 +1,13 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
 export function addCdn(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
+  return (tree: Tree, context: SchematicContext) => {
     const stylesPath = 'src/styles.css';
     const cdnLink =
       '@import url("https://kalees64.github.io/vk-cdn/vk-cdn.css");';
 
+    // Add the CDN link to styles.css
     if (tree.exists(stylesPath)) {
       const content = tree.read(stylesPath)?.toString('utf-8') || '';
       if (!content.includes(cdnLink)) {
@@ -15,6 +16,14 @@ export function addCdn(_options: any): Rule {
     } else {
       tree.create(stylesPath, cdnLink);
     }
+
+    // Add npm packages installation task
+    context.addTask(
+      new NodePackageInstallTask({
+        packageName:
+          '@fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/angular-fontawesome',
+      })
+    );
 
     return tree;
   };
